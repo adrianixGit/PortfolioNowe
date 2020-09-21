a94859ff8462fb2148d24b912565573d2e868dcf
adrianixGit <69733145+adrianixGit@users.noreply.github.com>
2020-09-21 14:32:48 +0200

    Dodanie wszystkich plikow

---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Portfolio Adrian Górniak</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark  fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Adrian Górniak</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/zdjecie.jpg" alt="" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">O mnie</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Umiejetności</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Projekty</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#kontakt">Kontakt</a></li>
+                   
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Górniak
+                        <span>Adrian</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Tutaj dowiesz się wszytskiego
+    
+                    </div>
+                    <!-- <p class="lead mb-5">AKTUALNIE JESTEM W TRAKCIE KURSU <strong>"THE COMPLETE WEB DEVELOPER COURSE"</strong>. UKOŃCZYLEM TECHNIKUM INFORMATYCZNE, 
+                        PRZYGODE Z TWORZENIEM STRON INTERNETOWYCH ZACZĄLEM TAK NA PRAWDE W MAJU 2020 ROKU. IDZIE MI CALKIEM NIEŹLE, SZYBKO SIE UCZĘ, 
+                        MYŚLĘ ZE JEST TO COŚ, CO CHCIALBYM ROBIĆ W ŻYCIU. CIESZY MNIE KAŻDA NOWO NAUCZONA RZECZ.
+                        <strong>SZUKAM PRACY</strong> W KTOREJ MÓGŁBYM SIE JESZCZE DUŻO NAUCZYĆ, 
+                        KTÓRA POZWOLI MI ROZWINĄĆ SIĘ JESZCZE BARDZIEJ, TAK ABY BYĆ JAK NAJLEPSZYM W TYM CO CHCĘ ROBIĆ!.</p> -->
+                    
+                    </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section " id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5 text-center">Umiejętności</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5 text-center">
+                        <div class="col-lg-4">
+                            <img src="assets/img/html.png">
+                            <div class="subheading mb-3">HTML</div>
+                            <p>Znam wszystkie informacje potrzebne do stworzenia szablonu strony. Struktura strony html, tabele, nagłówki, formularze, oraz wiele innych.</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="assets/img/css.png">
+                            <div class="subheading mb-3">CSS</div>
+                            <p>Znam wszytskie informacje potrzebne do stylizacji stron internetowych, za pomocą styli kaskadowych.
+                            </p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="assets/img/js.png">
+                            <div class="subheading mb-3">JavaScript</div>
+                            <p>Znam wszytskie podstawy potrzebne do interakcji ze stroną internetową, takie jak funkcje, zmienne, pętle i wiele innych.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5 text-center">
+                        <div class="col-lg-4">
+                            <img src="assets/img/jQuery.png">
+                            <div class="subheading mb-3">jQuery</div>
+                            <p>Znam wszystkie podstawy które pozwolą na jeszcze szybszą i potężniejszą interakcję ze stroną.</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="assets/img/bootstrap.png">
+                            <div class="subheading mb-3">Bootstrap</div>
+                            <p>Poznałem wszystkie zasoby Bootstrap, potrafie tworzyc atrakcyjne, resposnywne strony internetowe w szybki sposób.
+                            </p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="assets/img/mysql.png">
+                            <div class="subheading mb-3">MySQL</div>
+                            <p>Znam podsawty które pozwalają na przechowywanie użytkowników i wiele innych rzeczy w bazie.
+                            </p>
+                        </div>
+                        
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5 text-center">
+                        <div class="col-lg-4">
+                            <img src="assets/img/PHP.png">
+                            <div class="subheading mb-3">PHP</div>
+                            <p>Znam podstawy języka php, takie jak: Przetwarzanie formularzy oraz kontakt z bazą.</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="assets/img/github.png">
+                            <div class="subheading mb-3">GitHub</div>
+                            <p>Umiejętne korzystanie z systemu kontroli wersji.
+                            </p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Projekty-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5 text-center">Projekty</h2>
+
+                    <div class="row row-cols-1 row-cols-md-2 text-center">
+                        <div class="col mb-4">
+                          <div class="card h-100">
+                            <img src="assets/img/furazarcia.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">FuraŻarcia</h5>
+                              <p class="card-text">Strona internetowa foodtrucka.</p>
+                              <a class="btn btn-dark btn-lg" style="width:45%;" href="http://furazarcia.adriandevelop.pl/" role="button">DEMO</a>
+                              <a class="btn btn-dark btn-lg" style="width:45%;" href="https://github.com/adrianixGit/FuraZarcia.git" role="button">GitHub</a>
+                            </div>
+                          </div>
+        
+                        </div>
+                        <div class="col mb-4">
+                            <div class="card h-100">
+                              <img src="assets/img/czarne.jpg" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                <h5 class="card-title">Nie skończone</h5>
+                                <p class="card-text">Niedostępne</p>
+                                <a class="btn btn-dark btn-lg" style="width:45%;" href="#" role="button">DEMO</a>
+                                <a class="btn btn-dark btn-lg" style="width:45%;" href="#" role="button">GitHub</a>
+                              </div>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <section class="resume-section" id="kontakt">
+                <div class="resume-section-content">
+                    <h2 class="mb-5 text-center">Kontakt</h2>
+                    <div id="error"></div>
+                    <form method="POST">
+                        <div class="form-group">
+                          <p for="exampleFormControlInput1">Email</p>
+                          <input type="email" class="form-control text-center" id="email" name="email" placeholder="Podaj adres email">
+                        </div>
+                        <div class="form-group">
+                            <p for="exampleFormControlInput1">Temat</p>
+                            <input type="text" class="form-control" id="temat" name="temat">
+                          </div>
+                        <div class="form-group">
+                          <p for="exampleFormControlTextarea1">Treść</p>
+                          <textarea class="form-control" id="tresc" name="tresc" rows="3"></textarea>
+                        </div>
+                        <button type="submit"  class="btn btn-dark">Wyślij</button>
+                      </form>
+
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://github.com/adrianixGit"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://www.facebook.com/adrian.gorniak.90/"><i class="fab fa-facebook-f"></i></a>
+                    </div> 
+                </div>
+            </section>
+
+            <?php 
+
+                $error = "";
+                $success = "";
+
+                if($_POST){
+
+                    if(!$_POST['email']){
+
+                        $error .= "Proszę uzupełnić pole email</br>";
+
+                    }
+
+                    if(!$_POST['temat']){
+
+                        $error .= "Proszę uzupełnić pole temat</br>";
+
+                    }
+
+                    if(!$_POST['tresc']){
+
+                        $error .= "Proszę uzupełnić pole treść</br>";
+
+                    }
+
+                    if($error != ""){
+
+                        $error = '<div class="alert alert-danger" role="alert">Poniższe pola są puste:</br>'.$error.'</div>';
+
+                    }else {
+
+                        $emailTo = "adrian.gorniak97@op.pl";
+                        $subject = $_POST['temat'];
+                        $content = $_POST['tresc'];
+                        $headers = "From: ".$_POST['email'];
+
+                            if(mail($emailTo, $subject, $content, $headers)){
+
+                                $success = '<div class="alert alert-success" role="alert">Wiadomość została wysłana!</div>';
+
+                            } else {
+
+                                $error = '<div class="alert alert-danger" role="alert">Nie udało się wysłać wiadomości!</div>';
+
+                            }
+
+                    }
+
+
+                }
+
+            ?>
+
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+
+        <script type="text/JavaScript">
+        
+            $("form").submit(function(){
+
+                var error = "";
+
+                if($("#email").val() == ""){
+
+                    error += "Proszę uzupełnić pole email</br>";
+
+                }
+
+                if($("#temat").val() == ""){
+
+                    error += "Proszę uzupełnić pole temat</br>";
+
+                }
+
+                if($("#tresc").val() == ""){
+
+                    error += "Proszę uzupełnić pole tresc</br>";
+
+                }
+
+                if(error != ""){
+
+                    $("#error").html('<div class="alert alert-danger" role="alert">Poniższe pola są puste:</br>' + error + '</div>');
+
+                    return false;
+
+                }else {
+
+                    return true;
+
+                }
+
+
+            });
+        
+        </script>
+
+    </body>
+</html>
